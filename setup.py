541a5d13590e4b7ca9633afa60a26de10e5bc2a9
xiongzhen <6829673+xiongzhen@users.noreply.github.com>
2025-08-19 16:44:57 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="onesharepy",
+    version="0.0.1",
+    author="xiongzhen (公众号: 正月十九)",
+    description="Download shared personal onedrive files.",
+    packages=find_packages(),
+    python_requires=">=3.13",
+)
